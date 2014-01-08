abb61a78519ecd6885540e1373b40a0e3817cd24
wycc <wycca1@gmail.com>
2014-01-09 07:23:00 +0800

    Merge from the OZW. Add the missing ReleaseNodes.

--- cpp/src/Driver.cpp
+++ cpp/src/Driver.cpp
@@ -3860,6 +3860,7 @@ bool Driver::EnablePoll
 
 		// allow the poll thread to continue
 		m_pollMutex->Unlock();
+		ReleaseNodes();
 
 		Log::Write( LogLevel_Info, nodeId, "EnablePoll failed - value not found for node %d", nodeId );
 		return false;
